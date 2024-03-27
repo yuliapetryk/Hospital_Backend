93a3f22d6f8d84f8b3d6868d40065a5e07e72200
Yulia Petryk <yulya.petruk.03@gmail.com>
2024-03-27 18:40:20 +0200

    Create project

---
+++ src/main/java/db/GeneralDB.java
@@ -0,0 +1,21 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class GeneralDB {
+
+    public Statement setConnection() throws ClassNotFoundException, SQLException {
+
+        Class.forName("org.postgresql.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/hospital","postgres","2503");
+        conn.setAutoCommit(false);
+        Statement ps = conn.createStatement();
+        return ps;
+
+    }
+
+    
+}
